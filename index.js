fcd5b404efdd0e36949b4a0de4b2ff16cffa6ee2
h4roru 4zure <h4roru4zure@outlook.com>
2023-02-06 20:30:36 -0300

    haciendo el commit de prueba con un endpoint

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-
-
 const express = require('express');
 const app = express();
-const port =3000;
+const port =3500;
+
 app.get('/',(req,res)=>{
   res.send("Hello world");
+})
+
+app.get('/about',(req,res)=>{
+  res.send("nueva ruta about  ");
+})
+
+app.get('/contact',(req,res)=>{
+  res.send("nueva ruta contactanos   ");
+})
+app.get('/products',(req,res)=>{
+  res.json({
+    name  : "Producto 1",
+    price  :1010
+  });
 })
 
 app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
 });
+
 
