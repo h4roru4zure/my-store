651db19c46a52f4810b976e57752ba928feb89e4
h4roru 4zure <h4roru4zure@outlook.com>
2023-02-06 21:16:57 -0300

    usando el comamndo get para el endpoint de products y prodcuts/:id ademas de otros endpoints

--- index.js
+++ index.js
@@ -14,14 +14,38 @@ app.get('/contact',(req,res)=>{
   res.send("nueva ruta contactanos   ");
 })
 app.get('/products',(req,res)=>{
-  res.json({
+  res.json([{
     name  : "Producto 1",
-    price  :1010
-  });
+    price  :3010
+  },
+  {
+    name  : "Producto 2",
+    price  :1430
+  },
+  {
+    name  : "Producto 3",
+    price  :10320
+  }]);
 })
 
 app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
 });
 
+app.get ("/products/:id", (req, res)=>{
+  const {id}=req.params;
+  //const id = req.params.id;
+  console.log(id);
+  res.json({
+    id,
+    name  : "Producto 1",
+    price  :3010
+  });
+});
+app.get('/categories/:categoriesId/products/:productsId',(req, res)=>{
+  const {categoriesId,productsId}=req.params;
+  res.json({
+    categoriesId,
+    productsId});
+})
 
